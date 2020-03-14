e190f41acc4467a626f9f32a7a20c85bad58ee99
Hz <36901033+Zheng7426@users.noreply.github.com>
2020-03-13 22:18:56 -0400

    Update extraction.py
    genre column cleaning

--- extraction.py
+++ extraction.py
@@ -74,6 +74,30 @@ def clean_summary(x):
         return x
 
 
+def clean_topical_term(x):
+
+    x = x.replace(' ', '_')
+
+    regex1 = r"[$]\w+"
+
+    if re.search(regex1, x):
+        match = re.findall(regex1, x)
+        result = lst_to_str(match).replace('_', ' ')
+        return result
+    else:
+        return x
+
+
+def clean_genre(x):
+    regex = r"\ba\w+"
+    if re.search(regex, x):
+        match = re.search(regex, x)
+        result = match.group()
+        result = result.replace('a', '', 1)
+        return result
+    else:
+        return x
+
 
 
 if __name__ == '__main__':
@@ -91,6 +115,8 @@ if __name__ == '__main__':
     df2['Author'] = df2['Author'].astype(str).apply(lambda x: clean_author(x))
     df2['Title'] = df2['Title'].apply(lambda x: clean_title(x))
     df2['Summary'] = df2['Summary'].astype(str).apply(lambda x: clean_summary(x))
+    df2['Topical_Term'] = df2['Topical_Term'].astype(str).apply(lambda x: clean_topical_term(x))
+    df2['Genre'] = df2['Genre'].astype(str).apply(lambda x: clean_genre(x))
     print(df2)
 
 
