285da83397f62db2a66b177c8a9fcf69cff46132
zheng7426 <lilsam6670@gmail.com>
2020-05-17 22:35:05 -0400

    merged dataframe as output csv

--- data-processing/BorrowingData/borrowing_data_processing.py
+++ data-processing/BorrowingData/borrowing_data_processing.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv('./output_marc.csv', dtype=object)
+df = pd.read_csv('../Marc/output_marc.csv', dtype=object)
 df1 = df.dropna(subset=['ISBN', 'Title'])
 df2 = pd.read_csv('./BibUsageWithISBNAvailCopiesByMonth.csv', dtype=object, header=None,
                   names=['BibID', 'ISBN', 'Month', 'Year', 'NumOfAvailItems', 'NumOfCKOs'])
 df2 = df2.sort_values(['Year', 'Month'])
 df3 = pd.DataFrame.merge(df2, df1, on='ISBN')#, how='left'*/)
 
-print(df1.dtypes)
-print(df2.dtypes)
-print(df1.head())
+df3.to_csv('merged_hpl.csv', index=False)
+
---
+++ data-processing/BorrowingData/merged_hpl.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv('./merged_hpl.csv')
+print(df.head())
