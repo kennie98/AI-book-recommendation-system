0e53045aa38198919de60e1c3b0fe738e1058622
Ken Tsoi <ken.tsoi@menlolab.com>
2020-06-01 01:13:38 -0400

    - add marc output file setting in config.ini - remove ISBN None columns

--- data-processing/Marc/extraction.py
+++ data-processing/Marc/extraction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import re
 import marcformat
 
@@ -6,7 +7,9 @@ import marcformat
 class MarcExtractor(object):
     tag_marc_file = 'MARC_FILE'
     tag_filter_columns = 'FILTER_COLUMNS'
+    tag_marc_output_file = 'MARC_OUTPUT_FILE'
     marcFile = ''
+    marcOutFile = ''
     filteredColumns = []
     df = pd.DataFrame()
     df1 = pd.DataFrame()
@@ -22,21 +25,23 @@ class MarcExtractor(object):
         header = pd.DataFrame()
         for chunk in pd.read_csv(self.marcFile, chunksize=self.chunkSize, encoding='latin1'):
             if self.count == 0:
-                # header = chunk.columns
                 header = [i.zfill(3) for i in chunk.columns]
             chunk.columns = header
             self.count += 1
             print(self.count)
             self.df2 = self.__filterColumns(chunk)
             self.__processColumns()
+            self.df2 = self.__filterNone(self.df2)
+            self.df2 = self.__nullToNone(self.df2)
             if self.df1.empty:
                 self.df1 = self.df2
             else:
                 self.df1 = pd.concat([self.df1, self.df2])
 
     def __processConfigFile(self, config_file):
-        marc_file = re.compile('^'+self.tag_marc_file + '\s*\=\s*')
-        filter_columns = re.compile('^'+self.tag_filter_columns + '\s*\=\s*')
+        marc_file = re.compile('^' + self.tag_marc_file + '\s*\=\s*')
+        filter_columns = re.compile('^' + self.tag_filter_columns + '\s*\=\s*')
+        marc_out_file = re.compile('^' + self.tag_marc_output_file + '\s*\=\s*')
         with open(config_file, 'r') as configFile:
             for line in configFile.readlines():
                 if re.search(marc_file, line):
@@ -44,14 +49,15 @@ class MarcExtractor(object):
                 elif re.search(filter_columns, line):
                     self.filteredColumns.extend(line.split('=')[-1].strip().replace(' ', '').split(','))
                     self.filteredColumns = [i.zfill(3) for i in self.filteredColumns]
+                elif re.search(marc_out_file, line):
+                    self.marcOutFile = line.split('=')[-1].strip()
         configFile.close()
         pass
 
     def __filterColumns(self, df):
         df = df[self.filteredColumns]
         df.columns = [marcformat.marcColumnDirectory[i] for i in self.filteredColumns]
-        df.dropna(subset=['ISBN', 'Title'], inplace=True)
-        df = df.where(pd.notnull(df), None)
+        df = self.__filterNaN(df)
         return df
 
     def __processColumns(self):
@@ -80,9 +86,28 @@ class MarcExtractor(object):
     def getResultDF(self):
         return self.df1
 
+    def saveToOutFile(self):
+        self.df1.to_csv(self.marcOutFile, index=False)
+
     @staticmethod
     def __lst_to_str(s):
         return "".join(str(i) for i in s)
+
+    @staticmethod
+    def __filterNaN(df):
+        df.dropna(subset=['ISBN', 'Title'], inplace=True)
+        return df
+
+    @staticmethod
+    def __nullToNone(df):
+        df.where(pd.notnull(df), None)
+        return df
+
+    @staticmethod
+    def __filterNone(df):
+        df['ISBN'].replace(to_replace=[None], value=np.nan, inplace=True)
+        df.dropna(subset=['ISBN', 'Title'], inplace=True)
+        return df
 
     def __clean_isbn(self, x):
         regex = r"\ba\w+"
@@ -91,7 +116,7 @@ class MarcExtractor(object):
             result = match.group()
             result = result.replace('a', '')
             if result:
-              return result
+                return result
 
     def __clean_author(self, x):
         regex1 = r"[$a\s]\w+"
@@ -129,7 +154,8 @@ class MarcExtractor(object):
             match = re.search(regex, x)
             if match:
                 result = match.group()
-                result = result.replace('a', '', 1).replace('_____', '\'').replace('____', '.').replace('___', '-').replace(
+                result = result.replace('a', '', 1).replace('_____', '\'').replace('____', '.').replace('___',
+                                                                                                        '-').replace(
                     '__', ',_').replace('_', ' ')
                 return result
         else:
@@ -156,8 +182,6 @@ class MarcExtractor(object):
             elif result.index('$v') == 0:
                 result = result.replace('$v', '', 1).replace('$a', ',').replace('$a', ',')
             return result
-        # else:
-        #     return x
 
     def __topical_geographic(self, x):
         x = x.replace(' ', '_')
@@ -183,6 +207,6 @@ class MarcExtractor(object):
 if __name__ == '__main__':
     marcExtractor = MarcExtractor('../config.ini')
     marcExtractor.processDataSet()
+    marcExtractor.saveToOutFile()
     df = marcExtractor.getResultDF()
-    df.to_csv('../data/output_marc.csv', index=False)
     print(df)
