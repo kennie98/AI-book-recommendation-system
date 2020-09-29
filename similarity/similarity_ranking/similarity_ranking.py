5c1f7bf4acc5d6614a6485edff52733c21fe6f73
Ken Tsoi <ken.tsoi@menlolab.com>
2020-09-28 22:39:20 -0400

    change similarity_ranking.py to class

--- similarity/similarity_ranking/similarity_ranking.py
+++ similarity/similarity_ranking/similarity_ranking.py
@@ -2,54 +2,56 @@ import os
 import subprocess
 import re
 
-FASTTEXT = os.path.join(os.getcwd(), "fasttext")
-TITLE_RE_PATTERN = r'(?P<score>1|0.\d+)\s(\d+)\s(?P<title>((\S+\s?)+))'
 
+class SimilarityRanking(object):
+    FASTTEXT = os.path.join(os.getcwd(), "fasttext")
+    TITLE_RE_PATTERN = r'(?P<score>1|0.\d+)\s(\d+)\s(?P<title>((\S+\s?)+))'
 
-def loadModel(model, corpus, nbest):
-    return subprocess.Popen((FASTTEXT, 'nnSent', model, corpus, nbest),
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
+    def __init__(self):
+        pass
 
+    def loadModel(self, model, corpus, nbest):
+        return subprocess.Popen((self.FASTTEXT, 'nnSent', model, corpus, nbest),
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
 
-def similarityRanking(proc, title):
-    print("\n*** Find the similar books for ", title, " ***")
-    lines = []
-    proc.stdin.write(str.encode(title + '\n'))
-    proc.stdin.flush()
-    while True:
-        line = proc.stdout.readline()
-        if line == b'\n':
-            break
-        else:
-            lines.append(line.decode("utf-8").strip())
-    return lines
+    def similarityRanking(self, proc, title):
+        print("\n*** Find the similar books for ", title, " ***")
+        lines = []
+        proc.stdin.write(str.encode(title + '\n'))
+        proc.stdin.flush()
+        while True:
+            line = proc.stdout.readline()
+            if line == b'\n':
+                break
+            else:
+                lines.append(line.decode("utf-8").strip())
+        return lines
 
+    def terminateProc(self, proc):
+        proc.stdin.close()
+        proc.terminate()
+        print("***Process terminated***")
 
-def terminateProc(proc):
-    proc.stdin.close()
-    proc.terminate()
-    print("***Process terminated***")
-
-
-def printList(list):
-    for l in list:
-        match = re.match(TITLE_RE_PATTERN, l)
-        if match is not None:
-            print(match.groupdict())
+    def printList(self, list):
+        for l in list:
+            match = re.match(self.TITLE_RE_PATTERN, l)
+            if match is not None:
+                print(match.groupdict())
 
 
 if __name__ == '__main__':
-    proc = loadModel('model.bin', 'titleSet.txt', '30')
-    lines = similarityRanking(proc, "Animal Farm")
-    printList(lines)
-    lines = similarityRanking(proc, "Dracula")
-    printList(lines)
-    lines = similarityRanking(proc, "Frankenstein")
-    printList(lines)
-    lines = similarityRanking(proc, "pigs")
-    printList(lines)
-    lines = similarityRanking(proc, "The Bible")
-    printList(lines)
-    terminateProc(proc)
+    similarityRanker = SimilarityRanking()
+    proc = similarityRanker.loadModel('model.bin', 'titleSet.txt', '30')
+    lines = similarityRanker.similarityRanking(proc, "Animal Farm")
+    similarityRanker.printList(lines)
+    lines = similarityRanker.similarityRanking(proc, "Dracula")
+    similarityRanker.printList(lines)
+    lines = similarityRanker.similarityRanking(proc, "Frankenstein")
+    similarityRanker.printList(lines)
+    lines = similarityRanker.similarityRanking(proc, "pigs")
+    similarityRanker.printList(lines)
+    lines = similarityRanker.similarityRanking(proc, "The Bible")
+    similarityRanker.printList(lines)
+    similarityRanker.terminateProc(proc)
