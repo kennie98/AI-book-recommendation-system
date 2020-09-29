94c1a83e729e0841c3fa55c32f540b4061996062
Ken Tsoi <ken.tsoi@menlolab.com>
2020-09-28 22:12:55 -0400

    updated

--- data-processing/MongoDB/query.py
+++ data-processing/MongoDB/query.py
@@ -22,12 +22,38 @@ class MarcQuery:
     def getTopicalGeographicRecords(self, topic):
         return list(self.marc.find({'TopicalGeographic': topic}))
 
+    def getAllBookTitles(self):
+        return list(self.marc.find({}, {'Title': 1, '_id': 0}))
 
-if __name__ == '__main__':
+# get all the book titles and save to file
+def getBookTitles(ofile):
+    import codecs
+
     marcQuery = MarcQuery()
     # readers = marcQuery.getGenreRecords('Readers')
     # print(readers)
     # phtographs = marcQuery.getTopicalMainRecords('Photographs')
     # print(phtographs)
-    canada = marcQuery.getTopicalGeographicRecords('Canada')
-    print(canada)
+    # canada = marcQuery.getTopicalGeographicRecords('Canada')
+    # print(canada)
+    books = marcQuery.getAllBookTitles()
+
+    bookTitleSet = set()
+
+    with codecs.open(ofile, "w", "utf-8") as Of:
+        for book in books:
+            bookTitleSet.add(book['Title'].lower())
+        for book in bookTitleSet:
+            Of.write(book+'\n')
+        Of.close()
+    return
+
+if __name__ == '__main__':
+    # readers = marcQuery.getGenreRecords('Readers')
+    # print(readers)
+    # phtographs = marcQuery.getTopicalMainRecords('Photographs')
+    # print(phtographs)
+    # canada = marcQuery.getTopicalGeographicRecords('Canada')
+    # print(canada)
+    getBookTitles("titleSet.txt")
+
